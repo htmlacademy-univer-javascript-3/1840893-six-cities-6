afeb06605bd267c0c2b533349eb3d2e030182b9c
JuliyaEduardovna <juliya.eduardovna@gmail.com>
2026-03-11 20:39:34 +0700

    3.9. Маршрут по тайным тропам

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,9 +1,53 @@
 import MainPage from '../../pages/main-page/main-page';
+import { Route, BrowserRouter, Routes } from 'react-router-dom';
+import { HelmetProvider } from 'react-helmet-async';
+import { AppRoutes, Authorization } from '../../constants/constants';
+import Favorites from '../../pages/favorites/favorites';
+import Login from '../../pages/login/login';
+import NotFound from '../../pages/not-found/not-found';
+import Offer from '../../pages/offer/offer';
+import PrivateRoute from '../private-route/private-route';
 
 type AppProps = {
   count: number;
-}
+};
 
-export default function App({count} : AppProps) {
-  return <MainPage count={count} />;
+export default function App({ count }: AppProps) {
+  return (
+    <HelmetProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path={AppRoutes.Root} element={<MainPage count={count} />} />
+
+          <Route
+            path={AppRoutes.Favorites}
+            element={
+              <PrivateRoute
+                restrictedFor={Authorization.NoAuth}
+                redirectedTo={AppRoutes.Login}
+              >
+                <Favorites />
+              </PrivateRoute>
+            }
+          />
+
+          <Route
+            path={AppRoutes.Login}
+            element={
+              <PrivateRoute
+                restrictedFor={Authorization.Auth}
+                redirectedTo={AppRoutes.Root}
+              >
+                <Login />
+              </PrivateRoute>
+            }
+          />
+
+          <Route path={`${AppRoutes.Offer}/:offerId`} element={<Offer />} />
+
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </HelmetProvider>
+  );
 }
